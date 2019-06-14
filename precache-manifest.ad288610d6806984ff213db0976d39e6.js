1c5b2878c2adbbea9300296643d174590c49d2ad
aswanthkoleri <aswanth9495@gmail.com>
2019-06-14 18:12:58 +0530

    Updates

---
+++ precache-manifest.ad288610d6806984ff213db0976d39e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1f0d640c44febb41e88979232fde19a",
+    "url": "/Codeforces-Contest-Reminder/index.html"
+  },
+  {
+    "revision": "10f8af218eb82dfdfbac",
+    "url": "/Codeforces-Contest-Reminder/static/css/main.3bd14211.chunk.css"
+  },
+  {
+    "revision": "a348b86727865182ca58",
+    "url": "/Codeforces-Contest-Reminder/static/js/2.f33bd7ca.chunk.js"
+  },
+  {
+    "revision": "10f8af218eb82dfdfbac",
+    "url": "/Codeforces-Contest-Reminder/static/js/main.b7557863.chunk.js"
+  },
+  {
+    "revision": "dea4e0dda5eec667654a",
+    "url": "/Codeforces-Contest-Reminder/static/js/runtime~main.c02993fa.js"
+  }
+]);
