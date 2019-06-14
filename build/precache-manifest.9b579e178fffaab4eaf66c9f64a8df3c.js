b6bb4eda60b806e3daf4b184188bb88c0e6e467f
aswanthkoleri <aswanth9495@gmail.com>
2019-06-14 18:03:07 +0530

    Made the production build

---
+++ build/precache-manifest.9b579e178fffaab4eaf66c9f64a8df3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0256517393962265e168454a78c3920b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10f8af218eb82dfdfbac",
+    "url": "/static/css/main.3bd14211.chunk.css"
+  },
+  {
+    "revision": "a348b86727865182ca58",
+    "url": "/static/js/2.f33bd7ca.chunk.js"
+  },
+  {
+    "revision": "10f8af218eb82dfdfbac",
+    "url": "/static/js/main.b7557863.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
